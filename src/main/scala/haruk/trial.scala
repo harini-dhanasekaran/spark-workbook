d88556f48070e430522e0b15faba1687040b8565
Harini Dhanasekaran <63070913+harini-dhanasekaran@users.noreply.github.com>
2022-08-21 07:22:07 +0530

    Initial Commit

---
+++ src/main/scala/haruk/trial.scala
@@ -0,0 +1,29 @@
+package  haruk
+
+import org.apache.spark._
+import org.apache.log4j._
+
+object  trial {
+  def main(args: Array[String]) {
+
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Create a SparkContext using every core of the local machine
+    val sc = new SparkContext("local[*]", "test-1")
+
+    // Read each line of my book into an RDD
+    val input = sc.textFile("data/book.txt")
+
+    // Split into words separated by a space character
+    val words = input.flatMap(x => x.split(" "))
+
+    // Count up the occurrences of each word
+    val wordCounts = words.countByValue()
+
+    words.saveAsTextFile("result.txt")
+
+    // Print the results.
+    wordCounts.foreach(println)
+  }
+}
